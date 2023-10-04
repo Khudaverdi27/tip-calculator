4e3858e4c9d588b0070470c91eee96e7b729d012
Khudaverdi Shiralizade <xudaverdi.sirelizade@gmail.com>
2023-10-05 00:02:01 +0400

    updates input values for only number

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let inputCustom = document.querySelector(".inputCustom")
 let inputBill = document.querySelector(".inputBill")
 let countPerson = document.querySelector(".inputPerson ")
 let errorMessage = document.querySelector(".errorMessage")
+let errorMsg = document.querySelector(".errorMsg")
 let tipAmount = document.querySelector("#tipAmount")
 let totalPrice = document.querySelector("#totalPrice")
 let btnReset = document.querySelector(".btn-reset")
@@ -15,42 +16,48 @@ tip.forEach((tipButtons) => {
         let countPersonValue = countPerson.value
         let billValue = inputBill.value
         let discountValue = e.target.textContent.trim()
-        if (countPerson.value != "") {
-            //if tip buttons text content length =2
-            if (discountValue.length == 2) {
-                percentage = (discountValue.slice(0, 1) / 100) * billValue
+        if (!isNaN(billValue)) {
+            if (countPersonValue != "") {
+                //if tip buttons text content length =2
+                if (discountValue.length == 2) {
+                    percentage = (discountValue.slice(0, 1) / 100) * billValue
 
-                tipAmount.textContent = parseFloat((percentage / countPersonValue).toFixed(2))
-                total = billValue / countPersonValue + percentage / countPersonValue
-                totalPrice.textContent = total
-                //else tip buttons text content length 
+                    tipAmount.textContent = parseFloat((percentage / countPersonValue).toFixed(2))
+                    total = billValue / countPersonValue + percentage / countPersonValue
+                    totalPrice.textContent = total
+                    //else tip buttons text content length 
+                } else {
+                    percentage = (discountValue.slice(0, 2) / 100) * billValue;
+                    tipAmount.textContent = parseFloat((percentage / countPersonValue).toFixed(2))
+                    total = billValue / countPersonValue + percentage / countPersonValue
+                    //round number and take 2 numbers after dot
+                    totalPrice.textContent = parseFloat(total.toFixed(2))
+                }
             } else {
-                percentage = (discountValue.slice(0, 2) / 100) * billValue;
-                tipAmount.textContent = parseFloat((percentage / countPersonValue).toFixed(2))
-                total = billValue / countPersonValue + percentage / countPersonValue
-                //round number and take 2 numbers after dot
-                totalPrice.textContent = parseFloat(total.toFixed(2))
+                errorMessage.style.display = "block"
             }
         } else {
-            errorMessage.style.display = "block"
+            errorMsg.style.display = "block"
         }
-
     })
 })
 //Custom input calculation
 inputCustom.addEventListener("click", (e) => {
-    if (countPerson.value != "") {
-        if (e.target.value != "") {
-            let customValue = e.target.value
-            let PerAmount = (inputBill.value * customValue / 100) / countPerson.value;
-            let Pertotal = inputBill.value / countPerson.value + PerAmount
-            tipAmount.textContent = parseFloat(PerAmount.toFixed(2))
-            totalPrice.textContent = parseFloat(Pertotal.toFixed(2))
+    if (!isNaN(inputBill.value)) {
+        if (countPerson.value != "") {
+            if (e.target.value != "" && e.target.value / 1 == e.target.value) {
+                let customValue = e.target.value
+                let PerAmount = (inputBill.value * customValue / 100) / countPerson.value;
+                let Pertotal = inputBill.value / countPerson.value + PerAmount
+                tipAmount.textContent = parseFloat(PerAmount.toFixed(2))
+                totalPrice.textContent = parseFloat(Pertotal.toFixed(2))
+            }
+        } else {
+            errorMessage.style.display = "block"
         }
     } else {
-        errorMessage.style.display = "block"
+        errorMsg.style.display = "block"
     }
-
 })
 //reset button event
 btnReset.addEventListener("click", () => {
